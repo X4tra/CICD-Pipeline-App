fe585053bf91c21476bf7902fe75974974b9bc8e
X4tra <x4tra@localhost.localdomain>
2026-04-25 12:35:07 +0200

    Working final test

--- backend-test/test_main.py
+++ backend-test/test_main.py
@@ -11,4 +11,4 @@ def test_read_main():
 def test_status():
     response = client.get("/status")
     assert response.status_code == 200
-    assert response.json() == {"message": "Up and running!"}
+    assert response.json() == {"message": "Up and running!!"}
